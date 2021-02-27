e35d3ad2a9b18ffd178fb0163f59db1eb2f6e750
Jeff Hage <ownerejeff@gmail.com>
2021-02-27 18:28:28 -0500

    try a fingerprint approach

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -2,4 +2,5 @@ export {};
 
 declare global {
     const USER_AGENTS_STORE: KVNamespace;
+    const REQUEST_FINGERPRINTS: KVNamespace;
 }
--- src/handler.ts
+++ src/handler.ts
@@ -6,19 +6,44 @@
  * @returns {Promise<Response>}
  */
 export async function handleRequest(request: Request): Promise<Response> {
-    // Get userAgent if it exists
-    const userAgent = request.headers.get('User-Agent');
-    const hashedUserAgent = btoa(userAgent);
+    // Get browser fingerprint if it exists
+    const browserFingerprint = generateRequestFingerprint(request.headers);
+    const hashedFingerprint = btoa(JSON.stringify(browserFingerprint));
 
     // Get the number of times we've heard it
-    const count = Number((await USER_AGENTS_STORE.get(hashedUserAgent)) || 0);
+    const count = Number((await REQUEST_FINGERPRINTS.get(hashedFingerprint)) || 0);
 
-    // Update times seen
-    await USER_AGENTS_STORE.put(hashedUserAgent, String(count + 1));
+    // Update times seen using hashed value as key
+    await REQUEST_FINGERPRINTS.put(hashedFingerprint, String(count + 1));
 
     // Create json in text form
-    const json = JSON.stringify({ userAgent, count }, null, 4);
+    const json = JSON.stringify({ ...browserFingerprint, count }, null, 4);
 
-    // Respond with HTML
+    // Respond with JSON
     return new Response(json, { headers: { 'content-type': 'application/json' } });
+}
+
+/**
+ * Creates object with specified header values, if they exist
+ *
+ * @param {Headers} [headers=new Headers()]
+ * @returns {Record<string, string>}
+ */
+function generateRequestFingerprint(headers: Headers = new Headers()): Record<string, string> {
+    const headerMappings = {
+        userAgent: 'user-agent',
+        ip: 'cf-connecting-ip',
+        country: 'cf-ipcountry',
+        language: 'accept-language',
+    };
+
+    return Object.keys(headerMappings).reduce((accumulator, friendlyName) => {
+        const headerValue = headers.get(headerMappings[friendlyName]);
+
+        if (headerValue) {
+            accumulator[friendlyName] = headerValue;
+        }
+
+        return accumulator;
+    }, {});
 }
